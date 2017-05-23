7c00c56ef5731989a2dc5f554afe60aaaeb7964c
cuizhennan <cuizhennan@rongcloud.cn>
2017-05-23 19:34:38 +0800

    add .gitignore file

--- src/main/java/czn/proj/HelloWorldWeb.java
+++ src/main/java/czn/proj/HelloWorldWeb.java
@@ -3,24 +3,24 @@ package czn.proj;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class HelloWorldWeb {
 
-	private static final Logger logger = LogManager.getLogger(HelloWorldWeb.class);
+    private static final Logger logger = LogManager.getLogger(HelloWorldWeb.class);
 
-	@RequestMapping("/")
-	public String home() {
-		logger.info("Request Method [home] rest [/]");
+    @RequestMapping("/")
+    public String home() {
+        logger.info("Request Method [home] rest [/]");
 
-		return "Hello World!";
-	}
+        return "Hello World!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldWeb.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldWeb.class, args);
+    }
 }
